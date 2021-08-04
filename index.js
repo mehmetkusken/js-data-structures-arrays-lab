65de695851aecb8cd5c13eec2281f4feec214a8c
Mehmet <mehmetkusken@gmail.com>
2021-08-03 18:18:02 -0600

    commit tree d0dcf9bb032351dba3dc498c7c4e374bb5071f89

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendDriver = (name) => drivers.push(name);
+
+const destructivelyPrependDriver = (name) => drivers.unshift(name);
+
+const destructivelyRemoveLastDriver = () => drivers.pop();
+
+const destructivelyRemoveFirstDriver = () => drivers.shift();
+
+const appendDriver = name => {
+  return [...drivers, name];
+};
+
+function prependDriver(name) {
+  return [name, ...drivers];
+};
+
+function removeLastDriver() {
+  return drivers.slice(0, drivers.length-1);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1)
+}
